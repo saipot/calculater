b3a7c381a4fb1521e7bfc7aaad8e0cbdf0c1b6da
SALIAJA-COMPUTER <SALIAJA-COMPUTER@KN-COMPUTER.home>
2019-09-23 22:04:49 +0100

    Initial steps for calculator project

---
+++ src/Calculations/Array.java
@@ -0,0 +1,18 @@
+package Calculations;
+
+import java.util.LinkedList;
+
+public class Array {
+
+	public static void main(String[] args) {
+
+		LinkedList<String> names = new LinkedList<>();
+		names.add(0, "John");
+		names.add(1, "Peter");
+		names.add(2, "Dolly");
+		names.add(3, "Alex");
+		System.out.println("Print names:" + names);
+
+	}
+
+}
